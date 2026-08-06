568cfc1757d01682267892ab18d6206ff7dda593
Uwakwe Chukwuerika Paul <uwakwechukwuebukapaul@gmail.com>
2026-08-06 19:13:56 +0100

    Expose intelligence fusion engine

--- app/investigation/__init__.py
+++ app/investigation/__init__.py
@@ -6,10 +6,12 @@ Sentinel DNA Investigation Package
 from .investigation_context import InvestigationContext
 from .investigation_repository import InvestigationRepository
 from .investigation_service import InvestigationService
+from .intelligence_fusion import IntelligenceFusionEngine
 
 
 __all__ = [
     "InvestigationContext",
     "InvestigationRepository",
     "InvestigationService",
+    "IntelligenceFusionEngine",
 ]
